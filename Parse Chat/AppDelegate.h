ddfb9792c591276ec5c2478d8e3b49e4b2199cbd
OoreFasawe <oore95@gmail.com>
2022-06-26 13:55:56 -0700

    Initial Commit

---
+++ Parse Chat/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Parse Chat
+//
+//  Created by Oore Fasawe on 6/26/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Parse Chat/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Parse Chat
+//
+//  Created by Oore Fasawe on 6/26/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
